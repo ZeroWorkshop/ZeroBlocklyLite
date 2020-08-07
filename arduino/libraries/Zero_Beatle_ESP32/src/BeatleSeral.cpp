2e83497fe074ad440871fa608bc3f1a258f8e1fd
ZeroWorkshop <36092823+ZeroWorkshop@users.noreply.github.com>
2020-08-07 14:51:57 +0800

    Added beatleESP32 lib

---
+++ arduino/libraries/Zero_Beatle_ESP32/src/BeatleSeral.cpp
@@ -0,0 +1,424 @@
+#include "BeatleSerial.h"
+
+bool BeatleSerial::begin(Stream &stream)
+{
+  _serial = &stream;
+
+  sendStack.start_byte = beatleCMD::SB;
+  sendStack.version    = beatleCMD::VER;
+  sendStack.length     = beatleCMD::LEN;
+  sendStack.end_byte   = beatleCMD::EB;
+
+  recStack.start_byte  = beatleCMD::SB;
+  recStack.version     = beatleCMD::VER;
+  recStack.length      = beatleCMD::LEN;
+  recStack.end_byte    = beatleCMD::EB;
+
+  return true;
+}
+
+void BeatleSerial::progEEPROM(uint8_t prodY, uint8_t prodM, uint8_t prodD, uint8_t delY, uint8_t delM, uint8_t delD) {
+	sendStack.commandValue  = beatleCMD::SET_PRODDATE;
+	sendStack.feedbackValue = beatleCMD::NO_FEEDBACK;
+		
+	uint8_t tempMSB = ((prodY & 0x1F) << 3) | ((prodM >> 1) & 0x7);
+	uint8_t tempLSB = ((prodM & 0x1)  << 7) | (((prodD >> 2) & 0x1F) << 2);
+	 
+  sendStack.paramMSB = tempMSB;
+  sendStack.paramLSB = tempLSB;
+
+  findChecksum(&sendStack);
+  sendData();
+  
+  delay(10);
+  
+  sendStack.commandValue  = beatleCMD::WRITE_EEPROM;
+	sendStack.feedbackValue = beatleCMD::NO_FEEDBACK;
+		
+	tempMSB = ((delY & 0x1F) << 3) | ((delM >> 1) & 0x7);
+	tempLSB = ((delM & 0x1)  << 7) | (((delD >> 2) & 0x1F) << 2);
+	 
+  sendStack.paramMSB = tempMSB;
+  sendStack.paramLSB = tempLSB;
+
+  findChecksum(&sendStack);
+  sendData();
+}
+
+
+void BeatleSerial::resetBeatle() {
+  sendStack.commandValue  = beatleCMD::RESET_BEATLE;
+
+  sendStack.feedbackValue = beatleCMD::NO_FEEDBACK;
+  sendStack.paramMSB = 0;
+  sendStack.paramLSB = 0;
+
+  findChecksum(&sendStack);
+  sendData();
+}
+
+
+void BeatleSerial::runWheel(uint8_t direct, uint8_t speed) {
+  sendStack.commandValue  = beatleCMD::RUNWHEELS;
+
+  sendStack.feedbackValue = beatleCMD::NO_FEEDBACK;
+  sendStack.paramMSB = direct;
+  sendStack.paramLSB = speed;
+
+  findChecksum(&sendStack);
+  sendData();
+}
+
+
+
+void BeatleSerial::setLeftSpeed(uint8_t direct, uint8_t speed) {
+  sendStack.commandValue  = beatleCMD::RUNLEFT;
+  sendStack.feedbackValue = beatleCMD::NO_FEEDBACK;
+  sendStack.paramMSB = direct;
+  sendStack.paramLSB = speed;
+
+  findChecksum(&sendStack);
+  sendData();
+}
+
+
+void BeatleSerial::setLeftSpeed(int speeds) {
+	uint8_t direct;
+	uint8_t speed;
+  if(speeds >0)
+  {
+  		direct = beatleCMD::RUNFORWARD;
+  		speed = speeds;
+  }
+  else
+  {
+  		direct = beatleCMD::RUNBACKWARD;
+  		speed = abs(speeds);
+  }
+  
+  setLeftSpeed(direct,speed);
+}
+
+
+void BeatleSerial::setRightSpeed(uint8_t direct, uint8_t speed) {
+  sendStack.commandValue  = beatleCMD::RUNRIGHT;
+
+  sendStack.feedbackValue = beatleCMD::NO_FEEDBACK;
+  sendStack.paramMSB = direct;
+  sendStack.paramLSB = speed;
+
+  findChecksum(&sendStack);
+  sendData();
+}
+
+
+
+void BeatleSerial::setRightSpeed(int speeds) {
+	uint8_t direct;
+  uint8_t speed;
+  if(speeds >0)
+  {
+  		direct = beatleCMD::RUNFORWARD;
+  		speed = speeds;
+  }
+  else
+  {
+  		direct = beatleCMD::RUNBACKWARD;
+  		speed = abs(speeds);
+  }
+  
+  setRightSpeed(direct,speed);
+}
+
+void BeatleSerial::setSpeeds(int LeftSpeed, int RightSpeed) {
+	uint8_t left_direct,left_speed;
+	uint8_t right_direct,right_speed;
+	if(LeftSpeed >0)
+	{
+		left_direct = beatleCMD::RUNFORWARD;
+		left_speed = LeftSpeed;
+	}
+	else
+	{
+		left_direct = beatleCMD::RUNBACKWARD;
+		left_speed = abs(LeftSpeed);		
+	}
+	if(RightSpeed >0)
+	{
+		right_direct = beatleCMD::RUNFORWARD;
+		right_speed = RightSpeed;
+	}
+	else
+	{
+		right_direct = beatleCMD::RUNBACKWARD;
+		right_speed = abs(RightSpeed);		
+	}
+	
+	setLeftSpeed(left_direct, left_speed);
+	setRightSpeed(right_direct, right_speed);	
+}
+
+void BeatleSerial::playBuzzer() {
+  sendStack.commandValue  = beatleCMD::PLAYBUZZER;
+  sendStack.feedbackValue = beatleCMD::NO_FEEDBACK;
+  sendStack.paramMSB = 0;
+  sendStack.paramLSB = 1;
+
+  findChecksum(&sendStack);
+  sendData();
+}
+
+
+
+
+void BeatleSerial::setLED(uint8_t ID, uint8_t value) {
+  sendStack.commandValue  = beatleCMD::SETLED;
+  sendStack.feedbackValue = beatleCMD::NO_FEEDBACK;
+
+  sendStack.paramMSB = ID;
+  sendStack.paramLSB = value;
+
+
+  findChecksum(&sendStack);
+  sendData();
+}
+
+
+
+bool BeatleSerial::isDocked()
+{
+  int16_t result = query(beatleCMD::GET_STATUS_);
+
+  if (result != -1) {    //only when there is a feedback from the serial and the paraMSB == 0xCC and paraLSB == 0xDD, means the board is docked
+    if (recStack.paramMSB == 0xCC && recStack.paramLSB == 0xDD)  
+      return true;
+    else
+      return false;
+  }
+  else
+    return false;
+}
+
+
+
+int16_t BeatleSerial::buttonState(uint8_t button) {
+  return query(beatleCMD::GET_BUTTON_STATE, 0, button);
+}
+
+
+
+
+int16_t BeatleSerial::frontIRSensors() {
+  return query(beatleCMD::GET_IRSENSORS);
+}
+
+
+
+
+int16_t BeatleSerial::lineSensors() {
+  return query(beatleCMD::GET_LINESENSORS);
+}
+
+
+/////////////////////////////////////////////////////////
+void BeatleSerial::findChecksum(stack *_stack)
+{
+  uint16_t checksum = (~(_stack->version + _stack->length + _stack->commandValue + _stack->feedbackValue + _stack->paramMSB + _stack->paramLSB)) + 1;
+
+  _stack->checksumMSB = checksum >> 8;
+  _stack->checksumLSB = checksum & 0xFF;
+}
+
+
+
+
+void BeatleSerial::sendData()
+{
+  _serial->write(sendStack.start_byte);
+  _serial->write(sendStack.version);
+  _serial->write(sendStack.length);
+  _serial->write(sendStack.commandValue);
+  _serial->write(sendStack.feedbackValue);
+  _serial->write(sendStack.paramMSB);
+  _serial->write(sendStack.paramLSB);
+  _serial->write(sendStack.checksumMSB);
+  _serial->write(sendStack.checksumLSB);
+  _serial->write(sendStack.end_byte);
+}
+
+
+
+
+void BeatleSerial::flush()
+{
+  while (_serial->available())
+    _serial->read();
+}
+
+
+
+
+int16_t BeatleSerial::query(uint8_t cmd, uint8_t msb, uint8_t lsb)
+{
+  flush();
+
+  sendStack.commandValue  = cmd;
+  sendStack.feedbackValue = beatleCMD::FEEDBACK;
+  sendStack.paramMSB = msb;
+  sendStack.paramLSB = lsb;
+
+  findChecksum(&sendStack);
+  sendData();
+
+  if (getStatus(cmd))
+    return (recStack.paramMSB << 8) | recStack.paramLSB;
+
+  return -1;
+}
+
+
+
+
+bool BeatleSerial::getStatus(uint8_t cmd)
+{
+  timer = millis();
+  bool result = parseFeedback();
+
+  if (!result)
+    return false;
+
+  while (recStack.commandValue != cmd)
+  {
+    if (timeout())
+      return false;
+
+    result = parseFeedback();
+
+    if (!result)
+      return false;
+  }
+
+  return true;
+}
+
+
+
+
+bool BeatleSerial::parseFeedback()
+{                  
+	
+  while (_serial->available() < beatleCMD::STACK_SIZE)
+    if (timeout())  {    
+    	//Serial.println("TIMEOUT!!!!!!");
+      return false;
+    }
+
+  recStack.start_byte    = _serial->read();
+  recStack.version       = _serial->read();
+  recStack.length        = _serial->read();
+  recStack.commandValue  = _serial->read();
+  recStack.feedbackValue = _serial->read();
+  recStack.paramMSB      = _serial->read();
+  recStack.paramLSB      = _serial->read();
+  recStack.checksumMSB   = _serial->read();
+  recStack.checksumLSB   = _serial->read();
+  recStack.end_byte      = _serial->read();
+
+  //Serial.println(recStack.start_byte);
+  //Serial.println(recStack.version);
+  //Serial.println(recStack.length);
+  //Serial.println(recStack.commandValue);
+  //Serial.println(recStack.feedbackValue);
+  //Serial.println(recStack.paramMSB);
+  //Serial.println(recStack.paramLSB);
+  //Serial.println(recStack.checksumMSB);
+  //Serial.println(recStack.checksumLSB);
+  //Serial.println(recStack.end_byte);
+
+  
+  return true;
+}
+/*
+bool BeatleSerial::parseFeedback()
+{
+	uint8_t tempBuffer;
+  uint8_t cmdBuffer[beatleCMD::STACK_SIZE];
+  uint8_t tmp[beatleCMD::STACK_SIZE];
+  uint8_t index = 0;
+  uint8_t offset;
+  
+  while (_serial->available() < beatleCMD::STACK_SIZE)
+    if (timeout())
+      return false;
+      
+  while (_serial->available()) {
+    tempBuffer = _serial->read();
+    cmdBuffer[index++] = tempBuffer;
+  }
+
+  for (uint8_t i = 0; i < beatleCMD::STACK_SIZE; i++) {
+    if (cmdBuffer[i] == beatleCMD::SB &&
+        cmdBuffer[(i + 1) > 9 ? (i - 9) : (i + 1)] == beatleCMD::VER &&
+        cmdBuffer[(i + 2) > 9 ? (i - 8) : (i + 2)] == beatleCMD::LEN) {
+      offset = i;
+    }
+  }
+
+  for (uint8_t j = 0; j < beatleCMD::STACK_SIZE; j++) {
+    tmp[j] = cmdBuffer[offset + j > 9 ? (offset + j - 10) : (offset + j)];
+  }
+
+
+
+  recStack.start_byte    = tmp[0];
+  recStack.version       = tmp[1];
+  recStack.length        = tmp[2];
+  recStack.commandValue  = tmp[3];
+  recStack.feedbackValue = tmp[4];
+  recStack.paramMSB      = tmp[5];
+  recStack.paramLSB      = tmp[6];
+  recStack.checksumMSB   = tmp[7];
+  recStack.checksumLSB   = tmp[8];
+  recStack.end_byte      = tmp[9];
+  
+  Serial.write(recStack.start_byte);
+  Serial.write(recStack.version);
+  Serial.write(recStack.length);
+  Serial.write(recStack.commandValue);
+  Serial.write(recStack.feedbackValue);
+  Serial.write(recStack.paramMSB);
+  Serial.write(recStack.paramLSB);
+  Serial.write(recStack.checksumMSB);
+  Serial.write(recStack.checksumLSB);
+  Serial.write(recStack.end_byte);
+  return true;
+}
+*/
+
+
+
+bool BeatleSerial::timeout()
+{
+  if ((millis() - timer) >= threshold)
+    return true;
+  return false;
+}
+
+
+
+
+void BeatleSerial::printStack(stack _stack)
+{
+  Serial.println("Stack:");
+  Serial.print(_stack.start_byte, HEX);    Serial.print(' ');
+  Serial.print(_stack.version, HEX);       Serial.print(' ');
+  Serial.print(_stack.length, HEX);        Serial.print(' ');
+  Serial.print(_stack.commandValue, HEX);  Serial.print(' ');
+  Serial.print(_stack.feedbackValue, HEX); Serial.print(' ');
+  Serial.print(_stack.paramMSB, HEX);      Serial.print(' ');
+  Serial.print(_stack.paramLSB, HEX);      Serial.print(' ');
+  Serial.print(_stack.checksumMSB, HEX);   Serial.print(' ');
+  Serial.print(_stack.checksumLSB, HEX);   Serial.print(' ');
+  Serial.println(_stack.end_byte, HEX);
+}
+
---
+++ arduino/libraries/Zero_Beatle_ESP32/src/BeatleSerial.h
@@ -0,0 +1,189 @@
+#pragma once
+#include "Arduino.h"
+
+
+
+namespace beatleCMD
+{
+	//-------------------------------------------------------------------------------------//
+	// Packet Values
+	//-------------------------------------------------------------------------------------//
+	const uint8_t STACK_SIZE      = 10;   //total number of bytes in a stack/packet (same for cmds and queries)
+	const uint8_t SB              = 0x3E; //start byte
+	const uint8_t VER             = 0xFF; //version
+	const uint8_t LEN             = 0x6;  //number of bytes after "LEN" (except for checksum data and EB)
+	const uint8_t FEEDBACK        = 1;    //feedback requested
+	const uint8_t NO_FEEDBACK     = 0;    //no feedback requested
+	const uint8_t EB              = 0xEF; //end byte
+
+	//-------------------------------------------------------------------------------------//
+	// Control Command Values
+	//-------------------------------------------------------------------------------------//
+  const uint8_t RUNWHEELS       = 0x01;
+  const uint8_t RUNLEFT         = 0x02;
+  const uint8_t RUNRIGHT        = 0x03;
+  
+	const uint8_t PLAYBUZZER      = 0x11;
+	
+	const uint8_t SETLED          = 0x21;
+	const uint8_t SETLED_ID				= 0x22;
+	const uint8_t SETLED_RED			= 0x23;
+	const uint8_t SETLED_BLUE			= 0x24;
+	const uint8_t SETLED_GREEN		= 0x25;
+	const uint8_t EXELED          = 0x26;
+	
+	const uint8_t RESET_BEATLE    = 0x31;
+	const uint8_t WRITE_EEPROM    = 0x41;
+	const uint8_t SET_PRODDATE    = 0x51;
+	
+	
+  //-------------------------------------------------------------------------------------//
+  // Run Wheel Parameter values
+  //-------------------------------------------------------------------------------------//
+  const uint8_t RUNFORWARD      = 0x00;
+  const uint8_t RUNBACKWARD     = 0x01;
+  const uint8_t WHEELSPEED      = 0x02;
+  
+  
+  //-------------------------------------------------------------------------------------//
+  // LED color Parameter values
+  //-------------------------------------------------------------------------------------//
+  //const uint8_t LEDON           = 0x00;
+  //const uint8_t LEDOFF          = 0x01;
+  //const uint8_t LEDWHITE        = 0x02;
+  //const uint8_t LEDRED          = 0x03;
+  //const uint8_t LEDGREEN        = 0x04;
+  //const uint8_t LEDBLUE         = 0x05;
+  const uint8_t ALL_LED					= 0x0C;//12
+  
+  
+	//-------------------------------------------------------------------------------------//
+	// Query Command Values
+	//-------------------------------------------------------------------------------------//
+	const uint8_t GET_STATUS_      = 0x81;
+	const uint8_t GET_BUTTON_STATE = 0x82;
+	const uint8_t GET_IRSENSORS    = 0x83;
+	const uint8_t GET_LINESENSORS  = 0x84;
+  
+  //-------------------------------------------------------------------------------------//
+  // Query return Values
+  //-------------------------------------------------------------------------------------//
+  const uint8_t BUTTON_PRESSED   = 0xF0;
+  const uint8_t BUTTON_RELEASED  = 0xF1;
+  const uint8_t IR_SENSED        = 0xA0;
+  const uint8_t IR_UNSENSED      = 0xA1;
+  
+	//-------------------------------------------------------------------------------------//
+	// Line sensor Values
+	//-------------------------------------------------------------------------------------//
+	const uint16_t ALLWHITE       = 0x0000;  //b00000000 
+	const uint16_t ALLBLACK       = 0x007F;  //b01111111
+	const uint16_t ZEBRA_0        = 0x0055;  //b01010101
+	const uint16_t ZEBRA_1        = 0x002A;  //b00101010
+	const uint8_t  SENSORPASS     = 0;
+	const uint8_t  SENSORFAIL     = 1;
+	const uint8_t  WHITESEEN      = 0;
+	const uint8_t  BLACKSEEN      = 1;
+	
+	const uint8_t EQ_POP          = 1;
+	const uint8_t EQ_ROCK         = 2;
+	const uint8_t EQ_JAZZ         = 3;
+	const uint8_t EQ_CLASSIC      = 4;
+	const uint8_t EQ_BASE         = 5;
+	
+
+	//-------------------------------------------------------------------------------------//
+	// Mode Values
+	//-------------------------------------------------------------------------------------//
+	const uint8_t REPEAT          = 0;
+	const uint8_t FOLDER_REPEAT   = 1;
+	const uint8_t SINGLE_REPEAT   = 2;
+	const uint8_t RANDOM          = 3;
+
+	//-------------------------------------------------------------------------------------//
+	// Playback Source Values
+	//-------------------------------------------------------------------------------------//
+	const uint8_t U               = 1;
+	const uint8_t TF              = 2;
+	const uint8_t AUX             = 3;
+	const uint8_t SLEEP           = 4;
+	const uint8_t FLASH           = 5;
+
+	//-------------------------------------------------------------------------------------//
+	// Base Volume Adjust Value
+	//-------------------------------------------------------------------------------------//
+	const uint8_t VOL_ADJUST      = 0x10;
+
+	//-------------------------------------------------------------------------------------//
+	// Repeat Play Values
+	//-------------------------------------------------------------------------------------//
+	const uint8_t STOP_REPEAT     = 0;
+	const uint8_t START_REPEAT    = 1;
+}
+
+
+typedef struct
+{
+		uint8_t red;
+	  uint8_t green;
+	  uint8_t blue;	
+} rgb_data_type;
+	
+
+class BeatleSerial
+{
+public:
+	Stream* _serial;
+    
+	struct stack {
+		uint8_t start_byte;
+		uint8_t version;
+		uint8_t length;
+		uint8_t commandValue;
+		uint8_t feedbackValue;
+		uint8_t paramMSB;
+		uint8_t paramLSB;
+		uint8_t checksumMSB;
+		uint8_t checksumLSB;
+		uint8_t end_byte;
+	} sendStack, recStack;
+	
+	
+	rgb_data_type rgb_data[12];
+
+	unsigned long timer;
+	unsigned long threshold = 500;
+
+
+
+
+	bool begin(Stream& stream);
+	void progEEPROM(uint8_t prodY, uint8_t prodM, uint8_t prodD, uint8_t delY, uint8_t delM, uint8_t delD);
+	void resetBeatle();
+	void runWheel(uint8_t direct, uint8_t speed);
+	void playBuzzer();
+	void setLED(uint8_t on_off, uint8_t color);
+	void setSpeeds(int speedL,int speedR);
+	void setLeftSpeed(uint8_t direct, uint8_t speed);
+	void setRightSpeed(uint8_t direct, uint8_t speed);
+	void setLeftSpeed(int speeds);
+	void setRightSpeed(int speeds);
+	
+	bool isDocked();
+	int16_t buttonState(uint8_t button);
+	int16_t frontIRSensors();
+	int16_t lineSensors();
+	
+	
+	
+////////////////////////////////////////////////////////////////////
+	void findChecksum(stack *_stack);
+	void sendData();
+	void flush();
+	int16_t query(uint8_t cmd, uint8_t msb=0, uint8_t lsb=0);
+	bool getStatus(uint8_t cmd);
+	bool parseFeedback();
+	bool timeout();
+
+	void printStack(stack _stack);
+};
